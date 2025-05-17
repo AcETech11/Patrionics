802094a5d336947f2f0b44a6da63440b27427c89
AcETech11 <ugochukwuchidiadi2@gmail.com>
2025-05-17 20:56:34 +0100

    worked on the feature section

--- Components/Features.jsx
+++ Components/Features.jsx
@@ -56,24 +56,24 @@ const Features = () => {
           variants={container}
         >
           {[{
-            icon: <FaUnlockKeyhole className='text-4xl w-full text-secondary-light dark:text-secondary-dark' />,
+            icon: <FaUnlockKeyhole className='text-4xl  bg-secondary-light dark:bg-secondary-dark rounded-full p-2 text-background-light dark:text-background-dark' />,
             title: 'Secure Your Space with Our Advanced Access Control Systems',
             text: 'Our access control solutions ensure safety and convenience for your property.',
             action: <Link href='/'><Button className='bg-primary-light dark:bg-primary-dark'>Learn More</Button></Link>
           }, {
-            icon: <RiCalendarScheduleLine className='text-4xl w-full text-secondary-light dark:text-secondary-dark' />,
+            icon: <RiCalendarScheduleLine className='text-4xl  bg-secondary-light dark:bg-secondary-dark rounded-full p-2 text-background-light dark:text-background-dark' />,
             title: 'Streamline Workforce Management with Time and Attendance Solutions',
             text: 'Track employee hours effortlessly with our innovative time and attendance systems.',
             action: <SignInButton mode="modal" className='font-headline text-primary-light dark:text-primary-dark'>Sign In</SignInButton>
           }, {
-            icon: <BsDoorOpenFill className='text-4xl w-full text-secondary-light dark:text-secondary-dark' />,
+            icon: <BsDoorOpenFill className='text-4xl  bg-secondary-light dark:bg-secondary-dark rounded-full p-2 text-background-light dark:text-background-dark' />,
             title: 'Enhance Guest Experience with Our Hotel Door Lock Solutions',
             text: 'Upgrade your hospitality offering with secure, keyless, and modern access systems.',
             action: <Link href='/'><Button>Learn More</Button></Link>
           }].map((feature, i) => (
             <motion.div
               key={i}
-              className='text-center flex flex-col gap-2 border py-4 px-4 rounded-md shadow dark:shadow'
+              className=' flex flex-col gap-2 border py-4 px-4 rounded-md shadow dark:shadow'
               variants={item}
             >
               {feature.icon}
